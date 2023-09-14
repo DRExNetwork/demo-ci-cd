ecb8e473d11ae0967764b925e31d1895a918d33c
Ahron Sotomayor <ahronsz32@gmail.com>
2023-09-14 08:02:38 -0500

    lastchange

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hello wold!');
+  res.send('¡Hello woldd!');
 });
 
 app.listen(3000, () => {
